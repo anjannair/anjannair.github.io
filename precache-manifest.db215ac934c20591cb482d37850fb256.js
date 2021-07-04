5127226a9ca03d4cb8ad764fee13aa3921163fe6
Anjan Nair <22571613+anjannair@users.noreply.github.com>
2021-07-04 23:40:07 +0530

    Updates

---
+++ precache-manifest.db215ac934c20591cb482d37850fb256.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9f33f03e1d8020156dcfd2a0d72111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee60be66efd4f8bb3166",
+    "url": "/static/css/main.3b4a8120.chunk.css"
+  },
+  {
+    "revision": "317e461b8c5d7bfe52c1",
+    "url": "/static/js/2.211bf4c4.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.211bf4c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee60be66efd4f8bb3166",
+    "url": "/static/js/main.4f5d7fec.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "08ac15077279dd5649014227603f23f4",
+    "url": "/static/media/anjan.08ac1507.jpg"
+  },
+  {
+    "revision": "87c2b6b3cf013472af5d2db58c3f00df",
+    "url": "/static/media/resume.87c2b6b3.pdf"
+  }
+]);
